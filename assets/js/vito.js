2f536edd5cd9c7f689532acaaf08730630ff1047
Oguzhan Korucu <99058978@mydavinci.nl>
2019-09-24 20:45:39 +0200

    Update vito.js

--- assets/js/vito.js
+++ assets/js/vito.js
@@ -1,28 +1,11 @@
 alert("Goed, je hebt gekozen voor Vito's Storyline. Vito is het hoofd van het New Yorkse maffiafamilie Corleone. Tradities en respect zijn de codewoorden in deze wereld.")
 var naam = prompt("Wil je je naam wijzigen?", "Vito"); // Vraagt of je je naam wilt wijzigen
-<<<<<<< HEAD
-var v1 = prompt("Oke " + naam + ", je moet langs een van je partners om informatie te krijgen over een cartel. Ga je langs Pablo of Emillio?\n1) pablo\n)2)Emilio"); // V1 staat voor Vraag 1, die wordt als eerst gesteld aan de speler
+var v1 = prompt("Oke " + naam + ", je moet langs een van je partners om informatie te krijgen over een cartel. Ga je langs Pablo of Emillio?\n1) Pablo\n2)Emilio"); // V1 staat voor Vraag 1, die wordt als eerst gesteld aan de speler
 
 if (v1 == 1) {
-
-    prompt("Goed, je bent aangekomen bij Pablo. Doe je de deur open of ga je terug?\n1) ga terug\n2) Doe de deur open.");
+    prompt("Goed, je bent aangekomen bij Pablo. Doe je de deur open of ga je terug?\n1) Ga terug\n2) Doe de deur open.");
 
 } else if (v1 == 2) {
-
-    prompt("Emillio is nu niet bereikbaar.\n1) Ga terug.");
-=======
-var v1 = prompt("Oke " + naam + ", je moet langs een van je partners om informatie te krijgen over een cartel. Ga je langs Pablo of Emillio?"); // V1 staat voor Vraag 1, die wordt als eerst gesteld aan de speler
-var toegang; // Geeft je toegang tot plekken
-
-var pablov1 = prompt("Goed, je bent aangekomen bij Pablo. Doe je de deur open of ga je terug?");
-var emilliov1 = prompt("Emillio is nu niet bereikbaar.");
-
-if (v1 = "Pablo") {
-    toegang = true;
-    pablov1;
+    prompt("Emillio is nu niet bereikbaar.\n1) Ga terug.")
     
-} else if (v1 = "Emillio") {
-    toegang = false;
-    emilliov1;
->>>>>>> master
 }
